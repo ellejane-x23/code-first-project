9933950e17e783309872c28cd79e68671c2b536a
Katie <katie.boydy@gmail.com>
2018-03-23 22:01:14 +0000

    hacky fix to penguin and button moving around when eye added

--- pingu.js
+++ pingu.js
@@ -4,11 +4,23 @@ $(document).ready(function() {
     });
 });
 
+$(document).ready(function() {
+    $("strong").on("click", function() {
+        $("#pingu").toggleClass("a_up", 1000); // 1sec
+    });
+});
+
 
 function toggleImage() {
   var image = document.getElementById('fish');
   image.classList.toggle('hidden');
 }
 
+function toggleeye(){
+  var eye = document.getElementById('ee');
+  eye.classList.toggle('hidden');
+}
+
 var div = document.querySelector('strong');
 div.addEventListener('click', toggleImage);
+div.addEventListener('click', toggleeye);
