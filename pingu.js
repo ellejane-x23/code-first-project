a25055a99ae62081c69bc350adf988dc64bd2143
Katie <katie.boydy@gmail.com>
2018-03-23 21:12:55 +0000

    button no longer lurches about on clicking to feed the penguin

--- pingu.js
+++ pingu.js
@@ -1,13 +1,14 @@
+$(document).ready(function() {
+    $("strong").on("click", function() {
+        $("#butt").toggleClass("animate_up", 1000); // 1sec
+    });
+});
+
+
 function toggleImage() {
   var image = document.getElementById('fish');
   image.classList.toggle('hidden');
 }
 
-function movebut(){
-  var but = document.getElementById("butt");
-  but.style.top = "-100px";
-}
-
 var div = document.querySelector('strong');
 div.addEventListener('click', toggleImage);
-div.addEventListener('click', movebut);
